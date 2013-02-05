fe0f19dfe3d715129042ad91461cd2c6fcf84fe2
Evgeny Reznichenko <kusakyky@gmail.com>
2013-02-05 13:28:25 +0400

    removed sudo

--- ubuntu1204.sh
+++ ubuntu1204.sh
@@ -5,7 +5,7 @@ add-apt-repository ppa:chris-lea/node.js
 add-apt-repository ppa:chris-lea/redis-server
 add-apt-repository ppa:nginx/stable
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' > /etc/apt/sources.list.d/10gen.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 
 apt-get update
 apt-get upgrade
