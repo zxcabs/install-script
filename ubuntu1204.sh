25cbab50797bc7efe12d22d3f09a3dea04523240
Evgeny Reznichenko <kusakyky@gmail.com>
2013-02-05 13:30:21 +0400

    added "-y"

--- ubuntu1204.sh
+++ ubuntu1204.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt-get install python-software-properties
+apt-get -y update
+apt-get -y install python-software-properties python g++ make
 add-apt-repository ppa:chris-lea/node.js
 add-apt-repository ppa:chris-lea/redis-server
 add-apt-repository ppa:nginx/stable
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' > /etc/apt/sources.list.d/10gen.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+apt-key -y adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 
 apt-get update
-apt-get upgrade
-apt-get install nano git make gcc redis-server mongodb-10gen nginx nodejs-dev npm
+apt-get -y install nano git redis-server mongodb-10gen nginx nodejs-dev npm
